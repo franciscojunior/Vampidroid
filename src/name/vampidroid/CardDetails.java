d1237b3dbc6f4333ef23d2ddbca1b47a2c83ff3e
Francisco Figueiredo Jr <francisco@npgsql.org>
2011-03-16 03:09:17 +0000

    Added CardDetails Activity to show card details. Initial abstraction of database access. Added support for searching cards by name

--- src/name/vampidroid/CardDetails.java
+++ src/name/vampidroid/CardDetails.java
@@ -2,9 +2,16 @@ package name.vampidroid;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class CardDetails extends Activity {
+	
+	static String QUERY_LIBRARY = "select Name, Type, Clan, Discipline, CardText from library where _id = ";
+	static String QUERY_CRYPT = "select Name, Type, Clan, Disciplines, CardText from crypt where _id = ";
+	
 	/**
 	 * @see android.app.Activity#onCreate(Bundle)
 	 */
@@ -15,9 +22,54 @@ public class CardDetails extends Activity {
 		
 		Intent i = getIntent();
 		
-		String path = i.getData().getPath();
+		String deck = i.getStringExtra(VampiDroid.DECK_NAME);
+		long card_id = i.getLongExtra(VampiDroid.CARD_ID, 0);
+		
+		String query;
+		
+		if (deck.equals("library"))
+			query = QUERY_LIBRARY;
+		else
+			query = QUERY_CRYPT;
+		
 		
 		setContentView(R.layout.cryptcarddetails);
+		
+		SQLiteDatabase db = VampiDroid.getDatabase();
+		Cursor c = db.rawQuery(query + String.valueOf(card_id), null );
+		c.moveToFirst();
+		
+		String cardName = c.getString(0);
+		String cardType = c.getString(1);
+		String cardClan = c.getString(2);
+		String cardDisciplines = c.getString(3);
+		String cardText = c.getString(4);
+		
+		c.close();
+		db.close();
+		
+		
+		
+		TextView txt = (TextView) findViewById(R.id.txtCardName);
+		txt.setText(cardName);
+		
+
+		txt = (TextView) findViewById(R.id.txtCardType);
+		txt.setText(cardType);
+		
+
+		txt = (TextView) findViewById(R.id.txtCardClan);
+		txt.setText(cardClan);
+		
+
+		txt = (TextView) findViewById(R.id.txtCardDisciplines);
+		txt.setText(cardDisciplines);
+		
+
+		txt = (TextView) findViewById(R.id.txtCardText);
+		txt.setText(cardText);
+		
+		
 		
 		
 	}
--- src/name/vampidroid/VampiDroid.java
+++ src/name/vampidroid/VampiDroid.java
@@ -1,6 +1,7 @@
 package name.vampidroid;
 
 import android.app.AlertDialog;
+import android.app.SearchManager;
 import android.app.TabActivity;
 import android.content.DialogInterface;
 import android.content.Intent;
@@ -8,16 +9,47 @@ import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.net.Uri;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
-import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ListView;
 import android.widget.SimpleCursorAdapter;
 import android.widget.TabHost;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.TabHost.TabContentFactory;
 
 public class VampiDroid extends TabActivity {
+	
+	
+	public static String DECK_NAME = "deck_name";
+	public static String CARD_ID = "card_id";
 
-	public static SQLiteDatabase db;
+	
+	
+	
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		menu.add(Menu.NONE, Menu.FIRST+1, Menu.NONE, "Search")
+						.setIcon(android.R.drawable.ic_search_category_default);
+		
+		return(super.onCreateOptionsMenu(menu));
+	}
+	
+	
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+			case Menu.FIRST+1:
+				onSearchRequested(); 
+				return(true);
+			
+	
+		}
+
+		return(super.onOptionsItemSelected(item));
+	}
+	
 
 	/** Called when the activity is first created. */
     @Override
@@ -26,16 +58,27 @@ public class VampiDroid extends TabActivity {
         setContentView(R.layout.main);
 
         
-        db = SQLiteDatabase.openDatabase("/sdcard/VampiDroid.db", null, SQLiteDatabase.OPEN_READONLY);
-        boolean b = db.isOpen();
         
+        Intent intent = getIntent();
+        
+        
+        String queryCryptDatabase;
+        
+        
+        if (intent.getAction().equals(Intent.ACTION_SEARCH)) {
+			String query=intent.getStringExtra(SearchManager.QUERY).trim();
+			queryCryptDatabase = "select _id, Name from crypt where Name like '%" + query + "%'";
+		}
+        else
+        	queryCryptDatabase = "select _id, Name from crypt";
         
         
         // Fill crypt list.
         
         ListView listCrypt = (ListView) findViewById(R.id.ListViewCrypt);
         
-        Cursor c = db.rawQuery("select _id, Name from crypt", null);
+        SQLiteDatabase db = getDatabase();
+        Cursor c = db.rawQuery(queryCryptDatabase, null);
         
         SimpleCursorAdapter adapter = new SimpleCursorAdapter(this, R.layout.listitem, 
         		c, new String[] { "Name" }, new int[] {android.R.id.text1});
@@ -51,13 +94,15 @@ public class VampiDroid extends TabActivity {
 		            View v, int position, long id) {
 				
 				// TODO Auto-generated method stub
+        		SQLiteDatabase db = getDatabase();
 				Cursor c = db.rawQuery("select CardText from crypt where _id = " + String.valueOf(id), null );
 				c.moveToFirst();
 				String cardText = c.getString(0);
 				c.close();
+				db.close();
 				
 								
-				AlertDialog.Builder builder = new AlertDialog.Builder(v.getContext());
+				/*AlertDialog.Builder builder = new AlertDialog.Builder(v.getContext());
 				
 				builder.setMessage(cardText);
 				builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
@@ -70,10 +115,13 @@ public class VampiDroid extends TabActivity {
 				AlertDialog alertDialog = builder.create(); 
 				alertDialog.show();
 				
-				Intent cryptCardIntent = new Intent(v.getContext(), CardDetails.class );
-							
+				*/
 				
-				cryptCardIntent.setData(Uri.withAppendedPath(Uri.parse("vampidroid://library/id"), String.valueOf(id)));
+				Intent cryptCardIntent = new Intent(v.getContext(), CardDetails.class );
+				
+				cryptCardIntent.putExtra(DECK_NAME, "crypt");
+				cryptCardIntent.putExtra(CARD_ID, id);
+				
 				startActivity(cryptCardIntent);
 				
 				
@@ -98,15 +146,29 @@ public class VampiDroid extends TabActivity {
         
         // Fill crypt list.
         
+        String queryLibraryDatabase;
+        
+        
+        if (intent.getAction().equals(Intent.ACTION_SEARCH)) {
+			String query=intent.getStringExtra(SearchManager.QUERY).trim();
+			queryLibraryDatabase = "select _id, Name from library where Name like '%" + query + "%'";
+		}
+        else
+        	queryLibraryDatabase = "select _id, Name from library";
+        
+        
         ListView listLibrary = (ListView) findViewById(R.id.ListViewLibrary);
         
-        c = db.rawQuery("select _id, Name from library", null);
+        
+        c = db.rawQuery(queryLibraryDatabase, null);
         
         adapter = new SimpleCursorAdapter(this, R.layout.listitem, 
         		c, new String[] { "Name" }, new int[] {android.R.id.text1});
         
         
         listLibrary.setAdapter(adapter);
+        
+        db.close();
         
         
         listLibrary.setOnItemClickListener(new OnItemClickListener() {
@@ -115,12 +177,14 @@ public class VampiDroid extends TabActivity {
 		            View v, int position, long id) {
 				
 				// TODO Auto-generated method stub
+				SQLiteDatabase db = getDatabase();
 				Cursor c = db.rawQuery("select CardText from library where _id = " + String.valueOf(id), null );
 				c.moveToFirst();
 				String cardText = c.getString(0);
 				c.close();
+				db.close();
 				
-				AlertDialog.Builder builder = new AlertDialog.Builder(v.getContext());
+				/*AlertDialog.Builder builder = new AlertDialog.Builder(v.getContext());
 				
 				builder.setMessage(cardText);
 				builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
@@ -131,7 +195,17 @@ public class VampiDroid extends TabActivity {
         
         
 				AlertDialog alertDialog = builder.create();
-				alertDialog.show(); 
+				alertDialog.show();
+				
+				 */
+				
+				Intent cryptCardIntent = new Intent(v.getContext(), CardDetails.class );
+				
+				cryptCardIntent.putExtra(DECK_NAME, "library");
+				cryptCardIntent.putExtra(CARD_ID, id);
+				
+				startActivity(cryptCardIntent);
+				
 				
 
 				
@@ -147,15 +221,18 @@ public class VampiDroid extends TabActivity {
         
         mTabHost.addTab(mTabHost.newTabSpec("tab_library").setIndicator("Library").setContent(R.id.ListViewLibrary));
         
-        
         mTabHost.setCurrentTab(0);
     }
+
+
+	static SQLiteDatabase getDatabase() {
+		return SQLiteDatabase.openDatabase("/sdcard/VampiDroid.db", null, SQLiteDatabase.OPEN_READONLY);
+	}
 
 	@Override
 	protected void onDestroy() {
 		// TODO Auto-generated method stub
 		super.onDestroy();
 		
-		db.close();
 	}
 }
