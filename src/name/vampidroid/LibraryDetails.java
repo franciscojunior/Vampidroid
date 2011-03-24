bc831f375c1839f480ff91d04da3a4d065a9a5e6
rafaelb.barbosa@gmail.com <rafaelb.barbosa@gmail.com>
2011-03-24 16:23:13 +0000

    different classes for each screen

---
+++ src/name/vampidroid/LibraryDetails.java
@@ -0,0 +1,75 @@
+package name.vampidroid;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class LibraryDetails extends Activity {
+	
+	static String QUERY_LIBRARY = "select Name, Type, Clan, Discipline, CardText, PoolCost, BloodCost from library where _id = ";
+	
+	/**
+	 * @see android.app.Activity#onCreate(Bundle)
+	 */
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		// TODO Put your code here
+		
+		Intent i = getIntent();
+		
+		String deck = i.getStringExtra(VampiDroid.DECK_NAME);
+		long card_id = i.getLongExtra(VampiDroid.CARD_ID, 0);
+		
+		String query;
+		
+		query = QUERY_LIBRARY;
+		
+		setContentView(R.layout.librarycarddetails);
+		
+		SQLiteDatabase db = VampiDroid.getDatabase();
+		Cursor c = db.rawQuery(query + String.valueOf(card_id), null );
+		c.moveToFirst();
+		
+		String cardName = c.getString(0);
+		String cardType = c.getString(1);
+		String cardClan = c.getString(2);
+		String cardDisciplines = c.getString(3);
+		String cardText = c.getString(4);
+		String cardPoolCost = c.getString(5);
+		String cardBloodCost = c.getString(6);
+				
+		c.close();
+		db.close();
+		
+		
+		
+		TextView txt = (TextView) findViewById(R.id.txtCardName);
+		txt.setText(cardName);
+		
+		txt = (TextView) findViewById(R.id.txtCardPoolCost);
+		txt.setText(cardPoolCost);
+		
+		txt = (TextView) findViewById(R.id.txtCardBloodCost);
+		txt.setText(cardBloodCost);
+
+		txt = (TextView) findViewById(R.id.txtCardType);
+		txt.setText(cardType);
+		
+
+		txt = (TextView) findViewById(R.id.txtCardClan);
+		txt.setText(cardClan);
+		
+
+		txt = (TextView) findViewById(R.id.txtCardDisciplines);
+		txt.setText(cardDisciplines);
+		
+
+		txt = (TextView) findViewById(R.id.txtCardText);
+		txt.setText(cardText);
+		
+	}
+}
