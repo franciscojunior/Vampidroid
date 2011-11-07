3b559d781759f9a0788628342729c4449cf4a491
Francisco Figueiredo Jr <francisco@npgsql.org>
2011-11-07 13:04:57 +0000

    New FragmentActivity to show all decks in a list.

---
+++ src/name/vampidroid/DecksList.java
@@ -0,0 +1,75 @@
+package name.vampidroid;
+
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.view.MenuItem;
+import android.view.LayoutInflater;
+import android.view.View;
+
+public class DecksList extends FragmentActivity {
+
+	
+	
+	
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+
+		
+		setContentView(R.layout.deckslist);
+		
+
+	}
+	
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+			case android.R.id.home:
+				Intent intent = new Intent(this, VampiDroid.class);
+				intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+				startActivity(intent);
+		}
+		
+		return super.onOptionsItemSelected(item);
+	}
+	
+	
+	
+
+	
+	
+	private void showAddNewDeck() {
+		// TODO Auto-generated method stub
+
+		
+		// Displays about view...
+		LayoutInflater li = LayoutInflater.from(this);
+		View view = li.inflate(R.layout.new_deck, null);
+
+		new AlertDialog.Builder(this)
+				// .setTitle("New Deck")
+				// .setIcon(android.R.drawable.ic_menu_info_details)
+				.setView(view)
+				.setPositiveButton("Add deck",
+						new DialogInterface.OnClickListener() {
+							public void onClick(DialogInterface dialog,
+									int whichButton) {
+
+							}
+						})
+				.setNegativeButton("Cancel",
+						new DialogInterface.OnClickListener() {
+							public void onClick(DialogInterface dialog,
+									int whichButton) {
+								//
+							}
+						}).show();
+
+	}
+
+}
