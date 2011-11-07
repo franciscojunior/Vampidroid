3c6fb76f57083453ba40491bdd644305e3ce3555
Francisco Figueiredo Jr <francisco@npgsql.org>
2011-11-07 13:28:43 +0000

    Changed to use the new CardListCursorAdapter. Added support for updated favorite cards viewing. See OnResume method for more info. Removed contextmenu to add cards to favorites

--- src/name/vampidroid/fragments/LibraryListFragment.java
+++ src/name/vampidroid/fragments/LibraryListFragment.java
@@ -1,21 +1,16 @@
 package name.vampidroid.fragments;
 
 import name.vampidroid.DatabaseHelper;
-import name.vampidroid.R;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
-import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.ListFragment;
 import android.support.v4.view.Menu;
-import android.util.Log;
-import android.view.ContextMenu;
+import android.support.v4.widget.SimpleCursorAdapter;
 import android.view.MenuInflater;
-import android.view.MenuItem;
 import android.view.View;
-import android.widget.AdapterView;
+import android.widget.ListAdapter;
 import android.widget.ListView;
-import android.widget.SimpleCursorAdapter;
 
 public class LibraryListFragment extends ListFragment {
 
@@ -29,11 +24,9 @@ public class LibraryListFragment extends ListFragment {
 	OnLibraryCardSelectedListener mListener;
 
 	private boolean mHighlight;
-	
-	private static final int MENU_ADD_FAVORITES_ID = Menu.FIRST+3;
-	private static final int MENU_REMOVE_FAVORITES_ID = Menu.FIRST+4;
-	
-	
+
+	private static final int MENU_ADD_FAVORITES_ID = Menu.FIRST + 3;
+	private static final int MENU_REMOVE_FAVORITES_ID = Menu.FIRST + 4;
 
 	@Override
 	public void onViewCreated(View view, Bundle savedInstanceState) {
@@ -41,18 +34,18 @@ public class LibraryListFragment extends ListFragment {
 		super.onViewCreated(view, savedInstanceState);
 
 		getListView().setFastScrollEnabled(true);
-		
+
 		setEmptyText("No card match specified filter");
-		
+
 		if (mHighlight)
 			getListView().setChoiceMode(ListView.CHOICE_MODE_SINGLE);
 		else
 			getListView().setChoiceMode(ListView.CHOICE_MODE_NONE);
-		
-		//getListView().setBackgroundResource(R.color.Black);
-		
+
+		// getListView().setBackgroundResource(R.color.Black);
+
 		registerForContextMenu(getListView());
-		
+
 	}
 
 	// interface to communicate click events with containers.
@@ -65,23 +58,14 @@ public class LibraryListFragment extends ListFragment {
 
 	public void filterData(String filter) {
 
-		SQLiteDatabase db = DatabaseHelper.getDatabase(getActivity()
-				.getApplicationContext());
+		if (filter.trim().length() > 0) {
 
-		Cursor c = db.rawQuery(mQuery + filter, null);
+			SQLiteDatabase db = DatabaseHelper.getDatabase();
 
-		mAdapter.changeCursor(c);
+			Cursor c = db.rawQuery(mQuery + filter, null);
 
-	}
-
-	@Override
-	public void onAttach(FragmentActivity activity) {
-		// TODO Auto-generated method stub
-		super.onAttach(activity);
-
-		Log.d(TAG, "onAttach");
-		
-		
+			mAdapter.changeCursor(c);
+		}
 
 	}
 
@@ -108,7 +92,9 @@ public class LibraryListFragment extends ListFragment {
 	public void onDestroy() {
 		// TODO Auto-generated method stub
 		super.onDestroy();
-		mAdapter.getCursor().close();
+
+		if (mAdapter != null)
+			mAdapter.getCursor().close();
 		mAdapter = null;
 		setListAdapter(null);
 
@@ -118,131 +104,164 @@ public class LibraryListFragment extends ListFragment {
 
 		mQuery = libraryQuery;
 		refreshList();
-		
+
 	}
 
 	private void refreshList() {
-	
-		SQLiteDatabase db = DatabaseHelper.getDatabase(getActivity()
-				.getApplicationContext());
-		
-		Cursor c = db.rawQuery( mQuery, null);
-	
+
+		SQLiteDatabase db = DatabaseHelper.getDatabase();
+
+		Cursor c = db.rawQuery(mQuery, null);
+
 		mAdapter.changeCursor(c);
-	
-		
-	}
-	
-	@Override
-	public void onCreateContextMenu(ContextMenu menu, View v,
-			ContextMenu.ContextMenuInfo menuInfo) {
-		
-		//MenuInflater inflater = getActivity().getMenuInflater();
-		//inflater.inflate(R.menu.crypt_list_context_menu, menu);
-		
-		
-		AdapterView.AdapterContextMenuInfo info = (AdapterView.AdapterContextMenuInfo) menuInfo;
-			
-		long id = getListAdapter().getItemId(info.position);
-		
-		if (DatabaseHelper.containsLibraryFavorite(getActivity().getApplicationContext(), id))
-			menu.add(Menu.NONE, MENU_REMOVE_FAVORITES_ID, Menu.NONE, R.string.remove_favorite_card);
-		else
-			menu.add(Menu.NONE, MENU_ADD_FAVORITES_ID, Menu.NONE, R.string.add_favorite_card);
-		
-		
-		
-		
-		menu.setHeaderTitle("Crypt card options");
-		
+
 	}
 
-
-	@Override
-	public boolean onContextItemSelected(MenuItem item) {
-		AdapterView.AdapterContextMenuInfo info;
-		
-		info = (AdapterView.AdapterContextMenuInfo) item.getMenuInfo();
-		
-		long id;
-		switch (item.getItemId()) {
-		
-			case MENU_ADD_FAVORITES_ID:
-				
-				id = getListAdapter().getItemId(info.position);
-				
-				DatabaseHelper.addLibraryFavoriteCard(getActivity()
-						.getApplicationContext(), id);
-
-				return true;
-				
-			case MENU_REMOVE_FAVORITES_ID:
-				id = getListAdapter().getItemId(info.position);
-				
-				DatabaseHelper.removeLibraryFavoriteCard(getActivity()
-						.getApplicationContext(), id);
-
-				return true;
-
-		}
-
-		return super.onContextItemSelected(item);
-	}
-
-	
+	// @Override
+	// public void onCreateContextMenu(ContextMenu menu, View v,
+	// ContextMenu.ContextMenuInfo menuInfo) {
+	//
+	// //MenuInflater inflater = getActivity().getMenuInflater();
+	// //inflater.inflate(R.menu.crypt_list_context_menu, menu);
+	//
+	//
+	// AdapterView.AdapterContextMenuInfo info =
+	// (AdapterView.AdapterContextMenuInfo) menuInfo;
+	//
+	// long id = getListAdapter().getItemId(info.position);
+	//
+	// if
+	// (DatabaseHelper.containsLibraryFavorite(getActivity().getApplicationContext(),
+	// id))
+	// menu.add(Menu.NONE, MENU_REMOVE_FAVORITES_ID, Menu.NONE,
+	// R.string.remove_favorite_card);
+	// else
+	// menu.add(Menu.NONE, MENU_ADD_FAVORITES_ID, Menu.NONE,
+	// R.string.add_favorite_card);
+	//
+	//
+	//
+	//
+	// menu.setHeaderTitle("Library card options");
+	//
+	// }
+	//
+	//
+	// @Override
+	// public boolean onContextItemSelected(MenuItem item) {
+	// AdapterView.AdapterContextMenuInfo info;
+	//
+	// info = (AdapterView.AdapterContextMenuInfo) item.getMenuInfo();
+	//
+	// long id;
+	// switch (item.getItemId()) {
+	//
+	// case MENU_ADD_FAVORITES_ID:
+	//
+	// id = getListAdapter().getItemId(info.position);
+	//
+	// DatabaseHelper.addLibraryFavoriteCard(getActivity()
+	// .getApplicationContext(), id);
+	//
+	// return true;
+	//
+	// case MENU_REMOVE_FAVORITES_ID:
+	// id = getListAdapter().getItemId(info.position);
+	//
+	// DatabaseHelper.removeLibraryFavoriteCard(getActivity()
+	// .getApplicationContext(), id);
+	//
+	// return true;
+	//
+	// }
+	//
+	// return super.onContextItemSelected(item);
+	// }
 
 	public void setHighlightChoice(boolean highlight) {
 		// TODO Auto-generated method stub
-		
+
 		mHighlight = highlight;
-		
+
 	}
 
 	@Override
 	public void onSaveInstanceState(Bundle outState) {
 		// TODO Auto-generated method stub
 		super.onSaveInstanceState(outState);
-		
+
 		outState.putString("query", mQuery);
 	}
 
 	@Override
-		public void onCreate(Bundle savedInstanceState) {
-			// TODO Auto-generated method stub
-			super.onCreate(savedInstanceState);
-			
-			if (savedInstanceState != null)
-				mQuery = savedInstanceState.getString("query");
-			else
-				mQuery = getArguments().getString("LibraryQuery");
-			
+	public void setListAdapter(ListAdapter adapter) {
+		// TODO Auto-generated method stub
+		super.setListAdapter(adapter);
+		mAdapter = (SimpleCursorAdapter) adapter;
 
-			SQLiteDatabase db = DatabaseHelper.getDatabase(getActivity()
-					.getApplicationContext());
+	}
 
-			Cursor c = db.rawQuery(mQuery, null);
+	// @Override
+	// public void onCreate(Bundle savedInstanceState) {
+	// // TODO Auto-generated method stub
+	// super.onCreate(savedInstanceState);
+	//
+	// if (savedInstanceState != null)
+	// mQuery = savedInstanceState.getString("query");
+	// else
+	// mQuery = getArguments().getString("LibraryQuery");
+	//
+	// SQLiteDatabase db = DatabaseHelper.getDatabase();
+	//
+	// Cursor c = db.rawQuery(mQuery, null);
+	//
+	// // mAdapter = new SimpleCursorAdapter(getActivity()
+	// // .getApplicationContext(), R.layout.librarylistitem, c,
+	// // new String[] { "Name", "Type", "Clan", "Discipline" },
+	// // new int[] { R.id.txtCardName, R.id.txtCardType,
+	// // R.id.txtCardClan, R.id.txtCardDiscipline });
+	//
+	// mAdapter = new CardListCursorAdapter(CardType.LIBRARY, getActivity(),
+	// R.layout.librarylistitem, c, new String[] { "Name", "Type",
+	// "Clan", "Discipline" }, new int[] { R.id.txtCardName,
+	// R.id.txtCardType, R.id.txtCardClan,
+	// R.id.txtCardDiscipline });
+	//
+	// setListAdapter(mAdapter);
+	//
+	// try {
+	// mListener = (OnLibraryCardSelectedListener) getActivity();
+	// } catch (ClassCastException e) {
+	// throw new ClassCastException(getActivity().toString()
+	// + " must implement OnLibraryCardSelectedListener");
+	// }
+	//
+	// }
 
-			mAdapter = new SimpleCursorAdapter(getActivity()
-					.getApplicationContext(), R.layout.librarylistitem, c,
-					new String[] { "Name", "Type", "Clan", "Discipline" },
-					new int[] { R.id.txtCardName, R.id.txtCardType,
-							R.id.txtCardClan, R.id.txtCardDiscipline });
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
 
-			setListAdapter(mAdapter);
-			
-			
-			
-			
-			try {
-				mListener = (OnLibraryCardSelectedListener) getActivity();
-			} catch (ClassCastException e) {
-				throw new ClassCastException(getActivity().toString()
-						+ " must implement OnLibraryCardSelectedListener");
-			}
-	
-			
-			
+		if (savedInstanceState != null)
+			mQuery = savedInstanceState.getString("query");
+
+		try {
+			mListener = (OnLibraryCardSelectedListener) getActivity();
+		} catch (ClassCastException e) {
+			throw new ClassCastException(getActivity().toString() + " must implement OnLibraryCardSelectedListener");
 		}
-	
+
+	}
+
+	@Override
+	public void onResume() {
+		// TODO Auto-generated method stub
+		super.onResume();
+
+		// Favorite cards may have been changed. Update views to reflect changed
+		// favorite status.
+		getListView().invalidateViews();
+	}
 
 }
