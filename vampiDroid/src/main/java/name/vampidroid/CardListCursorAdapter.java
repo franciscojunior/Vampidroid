9ba3eab69d67b1910ce6aa5e20224163beb990fb
Francisco Figueiredo Jr <francisco@npgsql.org>
2015-04-09 15:27:30 -0300

    Add discipline images and started to adapt CardListCursorAdapter to use them.

--- vampiDroid/src/main/java/name/vampidroid/CardListCursorAdapter.java
+++ vampiDroid/src/main/java/name/vampidroid/CardListCursorAdapter.java
@@ -93,6 +93,16 @@ public class CardListCursorAdapter extends SimpleCursorAdapter {
 
 		vh.txtCardName = (TextView) v.findViewById(R.id.txtCardName);
 
+
+        vh.disciplineImageViews[0] = (ImageView) v.findViewById(R.id.imgDis1);
+        vh.disciplineImageViews[1] = (ImageView) v.findViewById(R.id.imgDis2);
+        vh.disciplineImageViews[2] = (ImageView) v.findViewById(R.id.imgDis3);
+        vh.disciplineImageViews[3] = (ImageView) v.findViewById(R.id.imgDis4);
+        vh.disciplineImageViews[4] = (ImageView) v.findViewById(R.id.imgDis5);
+        vh.disciplineImageViews[5] = (ImageView) v.findViewById(R.id.imgDis6);
+        vh.disciplineImageViews[6] = (ImageView) v.findViewById(R.id.imgDis7);
+        vh.disciplineImageViews[7] = (ImageView) v.findViewById(R.id.imgDis8);
+
 		v.setTag(vh);
 
 		return v;
@@ -127,13 +137,34 @@ public class CardListCursorAdapter extends SimpleCursorAdapter {
 		// TODO Auto-generated method stub
 		super.bindView(view, context, cursor);
 
+        ViewHolder viewHolder = (ViewHolder) view.getTag();
+
 		int advColumnIndex = cursor.getColumnIndex("Adv");
 		if (advColumnIndex != -1 && cursor.getString(advColumnIndex).length() > 0) {
-			ViewHolder viewHolder = (ViewHolder) view.getTag();
+
 
 			viewHolder.txtCardName.setText("Adv. " + viewHolder.txtCardName.getText());
-
 		}
+
+        viewHolder.clearDisciplineImageViews();
+
+        String[] disciplines = cursor.getString(2).split(" ");
+
+        int disIndex = 0;
+        for (String discipline : disciplines) {
+            if (discipline.compareTo("for") == 0 )
+                viewHolder.disciplineImageViews[disIndex].setImageResource(R.drawable.ic_dis_fortitude);
+            else if (discipline.compareTo("FOR") == 0 )
+                viewHolder.disciplineImageViews[disIndex].setImageResource(R.drawable.ic_dis_fortitude_sup);
+            else if (discipline.compareTo("cel") == 0 )
+                viewHolder.disciplineImageViews[disIndex].setImageResource(R.drawable.ic_dis_celerity);
+            else if (discipline.compareTo("CEL") == 0 )
+                viewHolder.disciplineImageViews[disIndex].setImageResource(R.drawable.ic_dis_celerity_sup);
+
+            disIndex++;
+        }
+
+
 
 	}
 
@@ -142,6 +173,20 @@ public class CardListCursorAdapter extends SimpleCursorAdapter {
 		public ImageView editDeckCardImageView;
 		public TextView txtCardName;
 
+        // Discipline images
+
+        public ImageView[] disciplineImageViews = new ImageView[8];
+
+        public void clearDisciplineImageViews() {
+            for (int i = 0; i < 8; i++) {
+                if (disciplineImageViews[i] != null)
+                    disciplineImageViews[i].setImageResource(0);
+            }
+        }
+
 	}
+
+
+
 
 }
