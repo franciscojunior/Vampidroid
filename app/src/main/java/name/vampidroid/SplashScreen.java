d7e299753581d6f1553dd5462c0d300578bb4947
Francisco Figueiredo Jr <francisco@npgsql.org>
2016-07-27 15:03:43 -0300

    Improve splash screen
    Implement splash screen according to https://www.bignerdranch.com/blog/splash-screens-the-right-way/

--- app/src/main/java/name/vampidroid/SplashScreen.java
+++ app/src/main/java/name/vampidroid/SplashScreen.java
@@ -1,12 +1,9 @@
 package name.vampidroid;
 
-import java.util.Arrays;
 
 import android.content.Intent;
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
-import android.util.Log;
 
 public class SplashScreen extends FragmentActivity {
 
@@ -16,70 +13,10 @@ public class SplashScreen extends FragmentActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
-		
-		
-		setContentView(R.layout.splashscreen);
 
+		startActivity(new Intent(this, VampiDroid.class));
 
-        new UpdateDatabaseOperation().execute();
-
-		
-//
-//		new Thread() {
-//
-//
-//			@Override
-//			public void run() {
-//
-//				// Initialize the database...
-//				DatabaseHelper.getDatabase();
-//
-//
-//				startActivity(new Intent(SplashScreen.this, VampiDroid.class));
-//
-//				SplashScreen.this.finish();
-//
-//			}
-//
-//
-//		}.start();
-//
-//
-		
-		
+		finish();
 	}
 
-
-	private class UpdateDatabaseOperation extends AsyncTask<Void, Void, Boolean> {
-
-		@Override
-		protected Boolean doInBackground(Void... params) {
-
-			try {
-				DatabaseHelper.getDatabase();
-			} catch (Exception e) {
-				return false;
-			}
-
-			return true;
-		}
-
-		@Override
-		protected void onPostExecute(Boolean result) {
-
-			Log.d(TAG, "onPostExecute... ");
-
-			if (result) {
-				startActivity(new Intent(SplashScreen.this, VampiDroid.class));
-
-				SplashScreen.this.finish();
-			}
-			else {
-
-			}
-
-
-		}
-	}
-	
 }
