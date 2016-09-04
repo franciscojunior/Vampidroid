89cce8b079f39df7b656a77bbf989a516788992e
Francisco Figueiredo Jr <francisco@npgsql.org>
2016-09-04 00:17:22 -0300

    Lot of optimizations to image loading and caching

--- app/src/main/java/name/vampidroid/Utils.java
+++ app/src/main/java/name/vampidroid/Utils.java
@@ -12,15 +12,15 @@ import android.graphics.drawable.Drawable;
 import android.os.AsyncTask;
 import android.os.Build;
 import android.support.v4.util.LruCache;
+import android.support.v4.util.SimpleArrayMap;
 import android.support.v7.graphics.drawable.DrawerArrowDrawable;
+import android.util.Log;
 import android.view.View;
 import android.view.animation.AccelerateDecelerateInterpolator;
 import android.widget.ImageView;
 
 import java.io.File;
 import java.text.Normalizer;
-import java.util.HashMap;
-
 
 
 /**
@@ -31,15 +31,15 @@ public class Utils {
 
     private static final String TAG = "Utils";
 
-    private static HashMap<String, Integer> disciplinesDrawableResourcesMap;
-    private static Resources resources;
+    static SimpleArrayMap<String, Integer> disciplinesDrawableResourceIdsMap;
+    static Resources resources;
 
-    public interface LoadCardImageAsync {
+    interface LoadCardImageAsync {
 
         public void onImageLoaded(BitmapDrawable image);
 
     }
-    private static String cardImagesPath;
+    static String cardImagesPath;
 
     public static void setResources(Resources resources) {
         Utils.resources = resources;
@@ -55,7 +55,7 @@ public class Utils {
 
 
     static void loadCardImageThumbnail(ImageView cardImageView, final String cardImageFileName, final int resIdFallbackCardImage) {
-        new LoadImageOperation(cardImageView, cardImageFileName, resIdFallbackCardImage, 8).execute();
+        new LoadImageOperation(cardImageView, cardImageFileName, resIdFallbackCardImage, 8, true).execute();
     }
 
     static void loadCardImage(ImageView cardImageView, final String cardImageFileName, final int resIdFallbackCardImage, LoadCardImageAsync callback) {
@@ -90,11 +90,11 @@ public class Utils {
 
     }
 
-    public static String getCardFileName(String cardName) {
+    static String getCardFileName(String cardName) {
         return getCardFileName(cardName, false);
     }
 
-    public static String getCardFileName(String cardName, boolean advanced) {
+    static String getCardFileName(String cardName, boolean advanced) {
 
         StringBuilder cardFileName = new StringBuilder();
 
@@ -112,7 +112,7 @@ public class Utils {
         return cardFileName.toString();
     }
 
-    public static void setupExpandLayout(final View header, final View layoutToExpand, final ImageView imgArrow) {
+    static void setupExpandLayout(final View header, final View layoutToExpand, final ImageView imgArrow) {
         header.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -143,7 +143,7 @@ public class Utils {
 
     }
 
-    public static void playDrawerToggleAnim(final DrawerArrowDrawable d) {
+    static void playDrawerToggleAnim(final DrawerArrowDrawable d) {
         float start = d.getProgress();
         float end = Math.abs(start - 1);
         if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
@@ -166,80 +166,83 @@ public class Utils {
             d.setProgress(end);
     }
 
-    public static HashMap<String, Integer> getDisciplineStringsDrawableResourcesMap() {
-        return disciplinesDrawableResourcesMap;
-    }
 
+    static LruCache<String, Drawable> disciplineDrawablesCache = new LruCache<String, Drawable>(15) {
+
+        @Override
+        protected void entryRemoved(boolean evicted, String key, Drawable oldValue, Drawable newValue) {
+            super.entryRemoved(evicted, key, oldValue, newValue);
+            ((BitmapDrawable) oldValue).getBitmap().recycle();
+        }
+    };
 
     static {
 
-        disciplinesDrawableResourcesMap = new HashMap<>();
+        disciplinesDrawableResourceIdsMap = new SimpleArrayMap<>();
 
-        disciplinesDrawableResourcesMap.put("abo", R.drawable.ic_dis_abombwe);
-        disciplinesDrawableResourcesMap.put("ABO", R.drawable.ic_dis_abombwe_sup);
-        disciplinesDrawableResourcesMap.put("ani", R.drawable.ic_dis_animalism);
-        disciplinesDrawableResourcesMap.put("ANI", R.drawable.ic_dis_animalism_sup);
-        disciplinesDrawableResourcesMap.put("aus", R.drawable.ic_dis_auspex);
-        disciplinesDrawableResourcesMap.put("AUS", R.drawable.ic_dis_auspex_sup);
-        disciplinesDrawableResourcesMap.put("cel", R.drawable.ic_dis_celerity);
-        disciplinesDrawableResourcesMap.put("CEL", R.drawable.ic_dis_celerity_sup);
-        disciplinesDrawableResourcesMap.put("chi", R.drawable.ic_dis_chimerstry);
-        disciplinesDrawableResourcesMap.put("CHI", R.drawable.ic_dis_chimerstry_sup);
-        disciplinesDrawableResourcesMap.put("dai", R.drawable.ic_dis_daimoinon);
-        disciplinesDrawableResourcesMap.put("DAI", R.drawable.ic_dis_daimoinon_sup);
-        disciplinesDrawableResourcesMap.put("dem", R.drawable.ic_dis_dementation);
-        disciplinesDrawableResourcesMap.put("DEM", R.drawable.ic_dis_dementation_sup);
-        disciplinesDrawableResourcesMap.put("dom", R.drawable.ic_dis_dominate);
-        disciplinesDrawableResourcesMap.put("DOM", R.drawable.ic_dis_dominate_sup);
-        disciplinesDrawableResourcesMap.put("for", R.drawable.ic_dis_fortitude);
-        disciplinesDrawableResourcesMap.put("FOR", R.drawable.ic_dis_fortitude_sup);
-        disciplinesDrawableResourcesMap.put("mel", R.drawable.ic_dis_melpominee);
-        disciplinesDrawableResourcesMap.put("MEL", R.drawable.ic_dis_melpominee_sup);
-        disciplinesDrawableResourcesMap.put("myt", R.drawable.ic_dis_mytherceria);
-        disciplinesDrawableResourcesMap.put("MYT", R.drawable.ic_dis_mytherceria_sup);
-        disciplinesDrawableResourcesMap.put("nec", R.drawable.ic_dis_necromancy);
-        disciplinesDrawableResourcesMap.put("NEC", R.drawable.ic_dis_necromancy_sup);
-        disciplinesDrawableResourcesMap.put("obe", R.drawable.ic_dis_obeah);
-        disciplinesDrawableResourcesMap.put("OBE", R.drawable.ic_dis_obeah_sup);
-        disciplinesDrawableResourcesMap.put("obf", R.drawable.ic_dis_obfuscate);
-        disciplinesDrawableResourcesMap.put("OBF", R.drawable.ic_dis_obfuscate_sup);
-        disciplinesDrawableResourcesMap.put("obt", R.drawable.ic_dis_obtenebration);
-        disciplinesDrawableResourcesMap.put("OBT", R.drawable.ic_dis_obtenebration_sup);
-        disciplinesDrawableResourcesMap.put("pot", R.drawable.ic_dis_potence);
-        disciplinesDrawableResourcesMap.put("POT", R.drawable.ic_dis_potence_sup);
-        disciplinesDrawableResourcesMap.put("pre", R.drawable.ic_dis_presence);
-        disciplinesDrawableResourcesMap.put("PRE", R.drawable.ic_dis_presence_sup);
-        disciplinesDrawableResourcesMap.put("pro", R.drawable.ic_dis_protean);
-        disciplinesDrawableResourcesMap.put("PRO", R.drawable.ic_dis_protean_sup);
-        disciplinesDrawableResourcesMap.put("qui", R.drawable.ic_dis_quietus);
-        disciplinesDrawableResourcesMap.put("QUI", R.drawable.ic_dis_quietus_sup);
-        disciplinesDrawableResourcesMap.put("san", R.drawable.ic_dis_sanguinus);
-        disciplinesDrawableResourcesMap.put("SAN", R.drawable.ic_dis_sanguinus_sup);
-        disciplinesDrawableResourcesMap.put("ser", R.drawable.ic_dis_serpentis);
-        disciplinesDrawableResourcesMap.put("SER", R.drawable.ic_dis_serpentis_sup);
-        disciplinesDrawableResourcesMap.put("spi", R.drawable.ic_dis_spiritus);
-        disciplinesDrawableResourcesMap.put("SPI", R.drawable.ic_dis_spiritus_sup);
-        disciplinesDrawableResourcesMap.put("tem", R.drawable.ic_dis_temporis);
-        disciplinesDrawableResourcesMap.put("TEM", R.drawable.ic_dis_temporis_sup);
-        disciplinesDrawableResourcesMap.put("thn", R.drawable.ic_dis_thanatosis);
-        disciplinesDrawableResourcesMap.put("THN", R.drawable.ic_dis_thanatosis_sup);
-        disciplinesDrawableResourcesMap.put("tha", R.drawable.ic_dis_thaumaturgy);
-        disciplinesDrawableResourcesMap.put("THA", R.drawable.ic_dis_thaumaturgy_sup);
-        disciplinesDrawableResourcesMap.put("val", R.drawable.ic_dis_valeren);
-        disciplinesDrawableResourcesMap.put("VAL", R.drawable.ic_dis_valeren_sup);
-        disciplinesDrawableResourcesMap.put("vic", R.drawable.ic_dis_vicissitude);
-        disciplinesDrawableResourcesMap.put("VIC", R.drawable.ic_dis_vicissitude_sup);
-        disciplinesDrawableResourcesMap.put("vis", R.drawable.ic_dis_visceratika);
-        disciplinesDrawableResourcesMap.put("VIS", R.drawable.ic_dis_visceratika_sup);
+        disciplinesDrawableResourceIdsMap.put("abo", R.drawable.ic_dis_abombwe);
+        disciplinesDrawableResourceIdsMap.put("ABO", R.drawable.ic_dis_abombwe_sup);
+        disciplinesDrawableResourceIdsMap.put("ani", R.drawable.ic_dis_animalism);
+        disciplinesDrawableResourceIdsMap.put("ANI", R.drawable.ic_dis_animalism_sup);
+        disciplinesDrawableResourceIdsMap.put("aus", R.drawable.ic_dis_auspex);
+        disciplinesDrawableResourceIdsMap.put("AUS", R.drawable.ic_dis_auspex_sup);
+        disciplinesDrawableResourceIdsMap.put("cel", R.drawable.ic_dis_celerity);
+        disciplinesDrawableResourceIdsMap.put("CEL", R.drawable.ic_dis_celerity_sup);
+        disciplinesDrawableResourceIdsMap.put("chi", R.drawable.ic_dis_chimerstry);
+        disciplinesDrawableResourceIdsMap.put("CHI", R.drawable.ic_dis_chimerstry_sup);
+        disciplinesDrawableResourceIdsMap.put("dai", R.drawable.ic_dis_daimoinon);
+        disciplinesDrawableResourceIdsMap.put("DAI", R.drawable.ic_dis_daimoinon_sup);
+        disciplinesDrawableResourceIdsMap.put("dem", R.drawable.ic_dis_dementation);
+        disciplinesDrawableResourceIdsMap.put("DEM", R.drawable.ic_dis_dementation_sup);
+        disciplinesDrawableResourceIdsMap.put("dom", R.drawable.ic_dis_dominate);
+        disciplinesDrawableResourceIdsMap.put("DOM", R.drawable.ic_dis_dominate_sup);
+        disciplinesDrawableResourceIdsMap.put("for", R.drawable.ic_dis_fortitude);
+        disciplinesDrawableResourceIdsMap.put("FOR", R.drawable.ic_dis_fortitude_sup);
+        disciplinesDrawableResourceIdsMap.put("mel", R.drawable.ic_dis_melpominee);
+        disciplinesDrawableResourceIdsMap.put("MEL", R.drawable.ic_dis_melpominee_sup);
+        disciplinesDrawableResourceIdsMap.put("myt", R.drawable.ic_dis_mytherceria);
+        disciplinesDrawableResourceIdsMap.put("MYT", R.drawable.ic_dis_mytherceria_sup);
+        disciplinesDrawableResourceIdsMap.put("nec", R.drawable.ic_dis_necromancy);
+        disciplinesDrawableResourceIdsMap.put("NEC", R.drawable.ic_dis_necromancy_sup);
+        disciplinesDrawableResourceIdsMap.put("obe", R.drawable.ic_dis_obeah);
+        disciplinesDrawableResourceIdsMap.put("OBE", R.drawable.ic_dis_obeah_sup);
+        disciplinesDrawableResourceIdsMap.put("obf", R.drawable.ic_dis_obfuscate);
+        disciplinesDrawableResourceIdsMap.put("OBF", R.drawable.ic_dis_obfuscate_sup);
+        disciplinesDrawableResourceIdsMap.put("obt", R.drawable.ic_dis_obtenebration);
+        disciplinesDrawableResourceIdsMap.put("OBT", R.drawable.ic_dis_obtenebration_sup);
+        disciplinesDrawableResourceIdsMap.put("pot", R.drawable.ic_dis_potence);
+        disciplinesDrawableResourceIdsMap.put("POT", R.drawable.ic_dis_potence_sup);
+        disciplinesDrawableResourceIdsMap.put("pre", R.drawable.ic_dis_presence);
+        disciplinesDrawableResourceIdsMap.put("PRE", R.drawable.ic_dis_presence_sup);
+        disciplinesDrawableResourceIdsMap.put("pro", R.drawable.ic_dis_protean);
+        disciplinesDrawableResourceIdsMap.put("PRO", R.drawable.ic_dis_protean_sup);
+        disciplinesDrawableResourceIdsMap.put("qui", R.drawable.ic_dis_quietus);
+        disciplinesDrawableResourceIdsMap.put("QUI", R.drawable.ic_dis_quietus_sup);
+        disciplinesDrawableResourceIdsMap.put("san", R.drawable.ic_dis_sanguinus);
+        disciplinesDrawableResourceIdsMap.put("SAN", R.drawable.ic_dis_sanguinus_sup);
+        disciplinesDrawableResourceIdsMap.put("ser", R.drawable.ic_dis_serpentis);
+        disciplinesDrawableResourceIdsMap.put("SER", R.drawable.ic_dis_serpentis_sup);
+        disciplinesDrawableResourceIdsMap.put("spi", R.drawable.ic_dis_spiritus);
+        disciplinesDrawableResourceIdsMap.put("SPI", R.drawable.ic_dis_spiritus_sup);
+        disciplinesDrawableResourceIdsMap.put("tem", R.drawable.ic_dis_temporis);
+        disciplinesDrawableResourceIdsMap.put("TEM", R.drawable.ic_dis_temporis_sup);
+        disciplinesDrawableResourceIdsMap.put("thn", R.drawable.ic_dis_thanatosis);
+        disciplinesDrawableResourceIdsMap.put("THN", R.drawable.ic_dis_thanatosis_sup);
+        disciplinesDrawableResourceIdsMap.put("tha", R.drawable.ic_dis_thaumaturgy);
+        disciplinesDrawableResourceIdsMap.put("THA", R.drawable.ic_dis_thaumaturgy_sup);
+        disciplinesDrawableResourceIdsMap.put("val", R.drawable.ic_dis_valeren);
+        disciplinesDrawableResourceIdsMap.put("VAL", R.drawable.ic_dis_valeren_sup);
+        disciplinesDrawableResourceIdsMap.put("vic", R.drawable.ic_dis_vicissitude);
+        disciplinesDrawableResourceIdsMap.put("VIC", R.drawable.ic_dis_vicissitude_sup);
+        disciplinesDrawableResourceIdsMap.put("vis", R.drawable.ic_dis_visceratika);
+        disciplinesDrawableResourceIdsMap.put("VIS", R.drawable.ic_dis_visceratika_sup);
     }
 
 
 
-    public static void updateDisciplineImages(Context context, ImageView[] disciplineImageViews, String cardDisciplines) {
+    static void updateDisciplineImages(Context context, ImageView[] disciplineImageViews, String cardDisciplines) {
         new Utils.UpdateDisciplineImagesOperation(context, disciplineImageViews).execute(cardDisciplines);
     }
-
-
 
 
     private static class LoadImageOperation extends AsyncTask<Void, Void, Void> {
@@ -252,36 +255,39 @@ public class Utils {
         private final int bitmapSampleSize;
         private BitmapDrawable bitmapDrawable;
         private final int resIdFallbackCardImage;
+        private boolean checkTag;
 
 
-        public LoadImageOperation(ImageView cardImageView, String cardImageFileName, int resIdFallbackCardImage, int inSampleSize) {
-            this(cardImageView, cardImageFileName, resIdFallbackCardImage, inSampleSize, null);
+        LoadImageOperation(ImageView cardImageView, String cardImageFileName, int resIdFallbackCardImage, int inSampleSize, boolean checkTag) {
+            this(cardImageView, cardImageFileName, resIdFallbackCardImage, inSampleSize, checkTag, null);
         }
 
-        public LoadImageOperation(ImageView cardImageView, String cardImageFileName, int resIdFallbackCardImage) {
+        LoadImageOperation(ImageView cardImageView, String cardImageFileName, int resIdFallbackCardImage) {
             this(cardImageView, cardImageFileName, resIdFallbackCardImage, null);
         }
 
-        public LoadImageOperation(ImageView cardImageView, String cardImageFileName, int resIdFallbackCardImage, LoadCardImageAsync callback) {
-            this(cardImageView, cardImageFileName, resIdFallbackCardImage, 1, callback);
+        LoadImageOperation(ImageView cardImageView, String cardImageFileName, int resIdFallbackCardImage, LoadCardImageAsync callback) {
+            this(cardImageView, cardImageFileName, resIdFallbackCardImage, 1, false, callback);
         }
 
-        public LoadImageOperation(ImageView cardImageView, String cardImageFileName, int resIdFallbackCardImage, int inSampleSize, LoadCardImageAsync callback) {
+        LoadImageOperation(ImageView cardImageView, String cardImageFileName, int resIdFallbackCardImage, int inSampleSize, boolean checkTag, LoadCardImageAsync callback) {
 
             this.cardImageView = cardImageView;
             this.cardImageFileName = cardImageFileName;
             this.resIdFallbackCardImage = resIdFallbackCardImage;
             this.callback = callback;
             this.bitmapSampleSize = inSampleSize;
+            this.checkTag = checkTag;
 
         }
-
 
         @Override
         protected Void doInBackground(Void... voids) {
 
 
-//            String cardFileName = cardImageFileName == null ? "" : getCardFileName(cardImageFileName) + ".jpg";
+            if (isCancelled()) {
+                Log.d(TAG, "doInBackground: doing nothing because the task was cancelled.");
+            }
 
             File imageFile = new File(cardImagesPath + "/" + cardImageFileName);
 
@@ -305,18 +311,27 @@ public class Utils {
         @Override
         protected void onPostExecute(Void aVoid) {
 
-
-            cardImageView.setImageDrawable(bitmapDrawable);
-            if (callback != null) {
-                callback.onImageLoaded(bitmapDrawable);
+            // // TODO: 03/09/16 improve code to avoid the decoding of the image too, not only the setting.
+            if (!checkTag || cardImageView.getTag().equals(cardImageFileName)) {
+                cardImageView.setImageDrawable(bitmapDrawable);
+                if (callback != null) {
+                    callback.onImageLoaded(bitmapDrawable);
+                }
             }
         }
+
+        @Override
+        protected void onCancelled() {
+            super.onCancelled();
+            Log.d(TAG, "onCancelled() called");
+        }
+
     }
 
 
-    public static class UpdateDisciplineImagesOperation extends AsyncTask<String, Void, String[]> {
+    static class UpdateDisciplineImagesOperation extends AsyncTask<String, Void, String[]> {
 
-        private static LruCache<String, Drawable> imageViewsDrawablesMap = new LruCache<>(30);
+
 
         private static BitmapFactory.Options options = new BitmapFactory.Options();
 
@@ -329,7 +344,7 @@ public class Utils {
             options.inSampleSize = 4;
         }
 
-        public UpdateDisciplineImagesOperation(Context context, ImageView[] imageViews) {
+        UpdateDisciplineImagesOperation(Context context, ImageView[] imageViews) {
 
             res = context.getResources();
             imageViewsToUpdate = imageViews;
@@ -347,12 +362,13 @@ public class Utils {
         }
 
         private void addImageToCache(String disciplineKey) {
-            if (imageViewsDrawablesMap.get(disciplineKey) == null) {
-                Integer resourceId = Utils.getDisciplineStringsDrawableResourcesMap().get(disciplineKey);
+            if (disciplineDrawablesCache.get(disciplineKey) == null) {
+                Integer resourceId = disciplinesDrawableResourceIdsMap.get(disciplineKey);
                 if (resourceId != null) {
-                    imageViewsDrawablesMap.put(disciplineKey, new BitmapDrawable(res, BitmapFactory.decodeResource(res, resourceId, options)));
+                    disciplineDrawablesCache.put(disciplineKey, new BitmapDrawable(res, BitmapFactory.decodeResource(res, resourceId, options)));
                 }
             }
+
         }
 
         @Override
@@ -360,8 +376,8 @@ public class Utils {
 
             int disIndex = 0;
             for (String discipline : disciplines) {
-                imageViewsToUpdate[disIndex].setImageDrawable(imageViewsDrawablesMap.get(discipline));
-                //viewHolder.disciplineImageViews[disIndex].setImageBitmap(imageViewsDrawablesMap.get(discipline));
+                imageViewsToUpdate[disIndex].setImageDrawable(disciplineDrawablesCache.get(discipline));
+                //viewHolder.disciplineImageViews[disIndex].setImageBitmap(disciplineDrawablesCache.get(discipline));
                 imageViewsToUpdate[disIndex].setVisibility(View.VISIBLE);
                 disIndex++;
 
@@ -371,7 +387,7 @@ public class Utils {
     }
 
     //    Reference: http://stackoverflow.com/questions/8276634/android-get-hosting-activity-from-a-view
-    public static Activity getActivity(Context fromContext) {
+    static Activity getActivity(Context fromContext) {
         while (fromContext instanceof ContextWrapper) {
             if (fromContext instanceof Activity) {
                 return (Activity)fromContext;
@@ -380,5 +396,6 @@ public class Utils {
         }
         return null;
     }
+
 }
 
