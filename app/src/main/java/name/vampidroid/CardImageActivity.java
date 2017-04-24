416ac0ef209e46d29cfbf5e0eba09182635d455f
Francisco Figueiredo Jr <francisco@npgsql.org>
2017-04-24 17:21:15 -0300

    Initial improvements to the card image activity.
    Make statusbar, navigationbar and toolbar translucent.

--- app/src/main/java/name/vampidroid/CardImageActivity.java
+++ app/src/main/java/name/vampidroid/CardImageActivity.java
@@ -54,22 +54,30 @@ public class CardImageActivity extends AppCompatActivity {
             }
         });
 
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
-            defaultUIOptions = getWindow().getDecorView().getSystemUiVisibility();
-        }
-
+//
 //        imageView.setOnClickListener(new View.OnClickListener() {
 //            @Override
 //            public void onClick(View view) {
 //
 //
+////                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
+////                    View decorView = getWindow().getDecorView();
+////                        decorView.setSystemUiVisibility(uiOptionsFullScreen);
+////
+////                    }
+//
+//                View decorView = getWindow().getDecorView();
+//                // Hide both the navigation bar and the status bar.
+//                // SYSTEM_UI_FLAG_FULLSCREEN is only available on Android 4.1 and higher, but as
+//                // a general rule, you should design your app to hide the status bar whenever you
+//                // hide the navigation bar.
+//                int uiOptions = View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+//                        | View.SYSTEM_UI_FLAG_FULLSCREEN
+//                        | View.SYSTEM_UI_FLAG_LAYOUT_STABLE;
 //                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
-//                    View decorView = getWindow().getDecorView();
-//                        decorView.setSystemUiVisibility(uiOptionsFullScreen);
-//
-//                    }
-//
+//                    decorView.setSystemUiVisibility(uiOptions);
 //                }
+//            }
 //
 //        });
 
