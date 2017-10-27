64dfa67deed4c9aa7157e5ca7b3fbd24ae60baa3
Francisco Figueiredo Jr <francisco@npgsql.org>
2017-10-27 10:05:40 -0200

    Add some explanations about the decision to use notifyDataSetChanged()
    As the number of items in the list is not big, it is simpler to rebind all data in order to reload the card image. Also, the code readability is better.

--- app/src/main/java/name/vampidroid/ViewPagerAdapter.java
+++ app/src/main/java/name/vampidroid/ViewPagerAdapter.java
@@ -100,8 +100,28 @@ public class ViewPagerAdapter extends PagerAdapter {
      * the images need to be reloaded. This is done by forcing the recyclerview to repopulate the data from the adapter.
      */
     public void refreshCardImages() {
+
+
+
+//        LinearLayoutManager cryptLayoutManager = (LinearLayoutManager) recyclerViews[0].getLayoutManager();
+//        LinearLayoutManager libraryLayoutManager = (LinearLayoutManager) recyclerViews[1].getLayoutManager();
+//
+//
+//        for (int i = cryptLayoutManager.findFirstVisibleItemPosition(); i < cryptLayoutManager.findLastVisibleItemPosition(); i++) {
+//            recyclerViewsAdapters[0].notifyItemChanged(i);
+//        }
+//
+//        for (int i = libraryLayoutManager.findFirstVisibleItemPosition(); i < libraryLayoutManager.findLastVisibleItemPosition(); i++) {
+//            recyclerViewsAdapters[1].notifyItemChanged(i);
+//        }
+
+        // TODO: 24/10/17 Check if it is worthwhile to implement it differently.
+        // As the number of items in the list is not big, it is simpler to rebind all data
+        // in order to reload the card image. Also, the code readability is better.
+
         recyclerViewsAdapters[0].notifyDataSetChanged();
         recyclerViewsAdapters[1].notifyDataSetChanged();
+
     }
 
 }
